223199dc994e4e95d4647df92251a6dbd33d37ec
oscarsick <oscarsick@gmail.com>
2018-11-27 15:12:51 -0800

    Update modellingTools.py

--- modellingTools.py
+++ modellingTools.py
@@ -1,5 +1,12 @@
 import maya.cmds as mc
 
-sel = mc.ls(sl = True)
-for each in sel:
-    print each
+#freezing all transform and move pivot to the origin
+def freeze2Zero():
+    sel = mc.ls(sl = True)
+    for each in sel:
+        mc.makeIdentity(each, a = True, s = True, t = True, r = True)
+        mc.move(0,0,0,each+'.scalePivot',a = True, rpr = True)
+        mc.move(0,0,0,each+'.rotatePivot',a = True, rpr = True)
+freeze2Zero()
+
+    
