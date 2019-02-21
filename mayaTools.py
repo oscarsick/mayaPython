dd954996ecc2c421c21841e14d1ce656f7c87d58
oscarsick <oscarsick@gmail.com>
2019-02-21 13:01:02 -0800

    Update mayaTools.py

--- mayaTools.py
+++ mayaTools.py
@@ -10,7 +10,25 @@ for each in sel:
     new_x = x.replace(old_v,v)
     mc.setAttr(each+'.fileTextureName',new_x,typ = 'string')
     
-    
+ 
+import maya.cmds as mc
+
+def os_mergeGroups():
+    sel = mc.ls(sl = True)
+    for each in sel:
+        c = mc.listRelatives(each,c = True, ad = True, type = 'transform')
+        geoArr = []
+        for child in c:
+            if '_geo' in child:
+                geoArr.append(child)
+        
+        print each+' : '+str(len(geoArr))
+        if len(geoArr) == 1:
+            mc.parent(geoArr[0], w=True)
+        else:
+            mc.polyUnite (each, ch = 0, mergeUVSets = 1, centerPivot = True, name = each)
+
+os_mergeGroups()
     
 
 
